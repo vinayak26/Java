b6c5b6c5531f552f06fd282e75f8916d8264a412
Vinayak Mishra <vinayakmishra26@gmail.com>
2023-09-30 21:41:13 +0530

    init

---
+++ Loops/forloops.java
@@ -0,0 +1,15 @@
+public class forloops {
+    public static void main(String[] args) {
+
+        for(int counter = 0; counter < 11;counter ++){
+            //counter ++ = counter = counter + 1
+            System.out.println(counter);
+        }
+
+
+
+    //  for(int counter=0;counter<3;counter = counter +1){
+    //     System.out.println("Hello World");
+    // }   
+    }
+}
---
+++ Loops/problem2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class problem2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int table = 0;
+
+        for(int i=1;i<=10;i++){
+
+            table = n*i;
+            System.out.println(table);
+        }
+        
+
+    }
+}
